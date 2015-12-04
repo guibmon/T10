7a471d1ea6b47e2d9ebbe7dd269f69f8595e9eb6
guibmon <guibmon@yahoo.com.br>
2015-12-03 22:31:38 -0200

    Create T10.java

---
+++ T10.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t10;
+import Visao.Principal;
+/**
+ *
+ * @author Guilherme
+ */
+public class T10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal frm = new Principal();
+        frm.setVisible(true);
+    }
+    
+}
